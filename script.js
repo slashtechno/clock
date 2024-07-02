70674d571c6c943ac6f8a0d74e95b16fdbb9bed6
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-07-02 21:43:49 +0200

    Finished tutorial

--- script.js
+++ script.js
@@ -3,13 +3,31 @@
 function setDate(secondHand, minHand, hourHand) {
     // Set the divs that are styled to be clock hands
     const currentTime = new Date();
-    const seconds = currentTime.getSeconds();
     // Add 90 to account for the offset in the CSS
     // 360 degrees in a circle
     // 60 seconds in a minute
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    // ${} is like an f-string in Python, but without the need for the f prefix
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const seconds = currentTime.getSeconds()
+    const secondDeg = (seconds / 60) * 360 + 90
+    secondHand.style.transform = `rotate(${secondDeg}deg)`
+  
+    const mins = currentTime.getMinutes()
+    const minsDeg = (mins / 60) * 360 + 90
+    minHand.style.transform = `rotate(${minsDeg}deg)`
+  
+    const hours = currentTime.getHours()
+    const hoursDeg = (hours / 12) * 360 + 90
+    hourHand.style.transform = `rotate(${hoursDeg}deg)`
+
+    // When the minute changes, reset the positions of the other hands instead of keeping the prior position before seamlessly transitioning
+    if (seconds == 0){
+        secondHand.style.transitionDuration = '0s';
+        minHand.style.transitionDuration = '0s';
+        hourHand.style.transitionDuration = '0s';
+    }{
+        secondHand.style.transitionDuration = '0.05s';
+        minHand.style.transitionDuration = '0.05s';
+        hourHand.style.transitionDuration = '0.05s';
+    };
 
     // Basically the same as setInterval but matches the refresh rate
     requestAnimationFrame(function(){
